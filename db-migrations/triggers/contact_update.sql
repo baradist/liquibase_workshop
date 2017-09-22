a96ded922f0cdaadad5e013d728dcf9b2d232f72
Oleg Grigoryev <oleg_grigoryev@epam.com>
2017-09-23 01:39:06 +0300

    ex. 8: Create a trigger

---
+++ db-migrations/triggers/contact_update.sql
@@ -0,0 +1,2 @@
+DROP TRIGGER IF EXISTS liquibase_workshop.contact_BEFORE_UPDATE;
+CREATE TRIGGER liquibase_workshop.contact_BEFORE_UPDATE BEFORE UPDATE ON contact FOR EACH ROW BEGIN SET NEW.full_name = CONCAT_WS(' ', NEW.givenname, NEW.middleinitial, NEW.surname); END;
